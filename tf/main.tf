9f050d16c74ebf867866e0330838465c839570d1
krudisar <krudisar@gmail.com>
2020-12-08 10:06:18 +0100

    added provider info

--- tf/main.tf
+++ tf/main.tf
@@ -1,5 +1,13 @@
 # demo content ...
 
+terraform {
+  required_providers {
+    dns = {
+      source = "hashicorp/aws"
+    }
+  }
+}
+
 provider "aws" {
   access_key = var.access_key
   secret_key = var.secret_key
