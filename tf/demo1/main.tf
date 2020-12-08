fdcaedc5259a25c542a717c1b73a2f6784c1ca54
krudisar <krudisar@gmail.com>
2020-12-08 11:17:38 +0100

    removed access info

--- tf/demo1/main.tf
+++ tf/demo1/main.tf
@@ -9,8 +9,8 @@ terraform {
 }
 
 provider "aws" {
-  access_key = var.access_key
-  secret_key = var.secret_key
+  #access_key = var.access_key
+  #secret_key = var.secret_key
   region = "eu-west-1"
 }
 
