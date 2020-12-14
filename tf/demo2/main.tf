d197a313798b76bac9d0ccff48d8f167e4bac077
krudisar <krudisar@gmail.com>
2020-12-14 09:52:30 +0100

    commented out aws_key_pair part

--- tf/demo2/main.tf
+++ tf/demo2/main.tf
@@ -58,8 +58,8 @@ resource "aws_instance" "ubuntu" {
   }
 }
 
-resource "aws_key_pair" "terraform_ec2_key" {
-  key_name = "terraform_ec2_key"
-  public_key = "${file("~/.ssh/id_rsa.pub")}"
-}
+#resource "aws_key_pair" "terraform_ec2_key" {
+#  key_name = "terraform_ec2_key"
+#  public_key = "${file("~/.ssh/id_rsa.pub")}"
+#}
 
