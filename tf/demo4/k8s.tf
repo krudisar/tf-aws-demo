328fb09a83dc6995796d8080ae21234eda70c215
krudisar <krudisar@gmail.com>
2021-02-09 15:47:10 +0100

    Update k8s.tf

--- tf/demo4/k8s.tf
+++ tf/demo4/k8s.tf
@@ -9,19 +9,10 @@ resource "azurerm_kubernetes_cluster" "k8s" {
     resource_group_name = "${azurerm_resource_group.k8s.name}"
     dns_prefix          = "${var.dns_prefix}"
 
-#    linux_profile {
-#        admin_username = "ubuntu"
-#
-#        ssh_key {
-#            key_data = "${file("${var.ssh_public_key}")}"
-#        }
-#    }
-
-    agent_pool_profile { 
+    default_node_pool { 
         name            = "agentpool"
-        count           = "${var.agent_count}"
+        node_count      = "${var.agent_count}"
         vm_size         = "Standard_DS1_v2"
-        os_type         = "Linux"
         os_disk_size_gb = 30
     }
 
@@ -30,6 +21,10 @@ resource "azurerm_kubernetes_cluster" "k8s" {
         client_secret = "${var.client_secret}"
     }
   
+    role_based_access_control {
+        enabled = true
+    }
+    
     tags = {
         Environment = "Development"
     }
